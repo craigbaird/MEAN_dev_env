2f5022fd9f5cf879cc362902b06e9ef36acde76a
Craig Baird <bairdcraig@gmail.com>
2017-08-27 16:31:51 -0500

    all authentication in place

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,14 @@
 var express = require("express");
 var router = express.Router();
-// var passport = require("passport");
+var passport = require("passport");
 var path = require("path");
 
-// router.post("/",
-//     passport.authenticate("local", {
-//         // request stays within node/express and is routed as a new request
-//         successRedirect: "/user",   // goes to routes/user.js
-//         failureRedirect: "/"        // goes to get "/" route below
-//     })
-// );
+router.post("/",
+    passport.authenticate("local", {
+        successRedirect: "/user",   // goes to routes/user.js
+        failureRedirect: "/"        // goes to get "/" route below
+    })
+);
 
 router.get("/", function(req, res) {
   console.log("request for index");
